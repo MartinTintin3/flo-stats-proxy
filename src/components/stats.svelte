42e1a6487b41ab7329acdf75940518ebe4dff9fb
Martin Maroyan <48408674+MartinTintin3@users.noreply.github.com>
2024-03-19 12:41:44 -0400

    Update stats.svelte

--- src/components/stats.svelte
+++ src/components/stats.svelte
@@ -4,6 +4,7 @@
 <span class="stats-data-field"><span class="stats-data-field-label">Pins:</span> {stats.pins}</span>
 <span class="stats-data-field"><span class="stats-data-field-label">Techs:</span> {stats.techs}</span>
 <span class="stats-data-field"><span class="stats-data-field-label">W/L Ratio:</span> <span class="green">{stats.ratio[0]}</span>:<span class="red">{stats.ratio[1]}</span> <span class="{stats.ratio[1] == 0 ? "green" : stats.ratio[0] / stats.ratio[1] > 1 ? "green" : "red"}">({stats.ratio[1] != 0 ? ((Math.round(stats.ratio[0] / stats.ratio[1] * 100) / 100).toFixed(2)) : stats.ratio[0]})</span></span>
+<span class="stats-data-field"><span class="stats-data-field-label">Win Percentage:</span class="{Math.round(stats.wins / stats.total * 100) > 50 ? "green" : "red"}"> <span>{((stats.wins / stats.total).toFixed(3) * 100).toFixed(1)}%</span></span>
 
 <script>
 	/** @type {import("../defs").Stats} */
